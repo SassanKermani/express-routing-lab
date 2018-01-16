dce773d00d5278854e660ba979eaeb119b079bd5
SassanKermani <34021065+SassanKermani@users.noreply.github.com>
2018-01-16 10:35:49 -0700

    Update candyRouter.js

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,61 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
-router = express.Router();
+var router = express.Router();
 
-var candies;
+var candies = [
+{"id":1,"name":"Chewing Gum","color":"Red"},
+{"id":2,"name":"Pez","color":"Green"},
+{"id":3,"name":"Marshmallow","color":"Pink"},
+{"id":4,"name":"Candy Stick","color":"Blue"},
+{"id": 5, "name":"Jelly Belly","color":"Orange"}];
 
 //What would need to go into candies
 //in order to pass our first test?
 
+//delete
+router.delete('/:id', function(req, res){
+	candies[req.params.id - 1] = null;
+});
+
+// //making a candy
+router.post('/', function(req, res) {
+
+	let tempVar = req.body;
+
+	console.log(tempVar); 
+
+	console.log("you are at post");
+    candies.push(tempVar);
+    res.send(tempVar);
+  });
+
+//updateing a candy
+router.put('/:id', function(req, res){
+	console.log("at post");
+	console.log(req.body);
+
+	console.log(req.params.id)
+	req.body.id = req.params.id;
+	candies[req.params.id - 1] = req.body;
+	res.send(candies);
+
+});
+
+//seeing one candy
+router.get('/:id', function(req,res) {
+	creq.body.id = candies.length + 1;
+	candies.push(req.body);
+	res.send(req.body);
+});
+
+//seeing all the candy
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+	console.log("you are at candyRouter");
+	res.send( candies );
 });
 
 // Fill out the rest of the routes here
 
 module.exports = router;
+
